19522415ae042a67976f6a2b431136cbd0418b3e
Adrien Vannson <adrien.vannson@protonmail.com>
2022-12-19 18:32:00 +0100

    Write the image in a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let width = 640;
+    let height = 360;
+
+    let mut output: image::RgbImage = image::ImageBuffer::new(width, height);
+
+    for x in 0..width {
+        for y in 0..height {
+            output.put_pixel(x, y, image::Rgb([255, 0, 0]));
+        }
+    }
+
+    output.save("output.png").expect("Could not save the image");
 }
