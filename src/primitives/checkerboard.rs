4700e68aba7b783b37804d385b3fa6002f296419
Adrien Vannson <adrien.vannson@protonmail.com>
2022-12-23 22:46:00 +0100

    Make the checkerboard a real checkerboard

--- src/primitives/checkerboard.rs
+++ src/primitives/checkerboard.rs
@@ -6,19 +6,24 @@ use crate::shape::Collision;
 use crate::vect::Vect;
 
 /// The checkboard is necessary horizontal
+/// The lines are along the x axis, and the columns along the y axis
 pub struct Checkerboard {
     origin: Vect,
     width: f64,
     height: f64,
+    lines_count: i32,
+    columns_count: i32,
     colors: [Color; 2],
 }
 
 impl Checkerboard {
-    pub fn new(origin: Vect, width: f64, height: f64, color1: Color, color2: Color) -> Self {
+    pub fn new(origin: Vect, width: f64, height: f64, lines_count: i32, columns_count: i32, color1: Color, color2: Color) -> Self {
         Checkerboard {
             origin,
             width,
             height,
+            lines_count,
+            columns_count,
             colors: [color1, color2],
         }
     }
@@ -59,9 +64,12 @@ impl Primitive for Checkerboard {
         }
     }
 
-    fn material_at_collition(&self, _collision: Collision) -> Material {
+    fn material_at_collition(&self, collision: Collision) -> Material {
+        let index_x = ((collision.pos.x - self.origin.x) / self.width * self.columns_count as f64) as usize;
+        let index_y = ((collision.pos.y - self.origin.y) / self.height * self.lines_count as f64) as usize;
+
         Material {
-            color: self.colors[0],
+            color: self.colors[((index_x + index_y) % 2) as usize],
         }
     }
 }
