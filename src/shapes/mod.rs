aaef5c77c3a94cb4b4f0266c015ce5dfddb3a3ef
Adrien Vannson <adrien.vannson@protonmail.com>
2023-04-23 23:35:37 +0200

    Add pub use

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -2,3 +2,7 @@ pub mod implicit_shape;
 pub mod implicit_shapes;
 pub mod sphere;
 pub mod intersection;
+
+pub use implicit_shape::ImplicitShape;
+pub use sphere::Sphere;
+pub use intersection::Intersection;
