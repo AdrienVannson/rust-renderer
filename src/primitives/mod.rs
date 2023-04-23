86cf3d8c31157bef9701d4b4a53b4c1e913cff99
Adrien Vannson <adrien.vannson@protonmail.com>
2023-04-23 23:38:05 +0200

    Add pub use

--- src/primitives/mod.rs
+++ src/primitives/mod.rs
@@ -1,3 +1,7 @@
 pub mod checkerboard;
 pub mod geometric_primitive;
 pub mod transformed_primitive;
+
+pub use checkerboard::Checkerboard;
+pub use geometric_primitive::GeometricPrimitive;
+pub use transformed_primitive::TransformedPrimitive;
--- src/renderers/mod.rs
+++ src/renderers/mod.rs
@@ -1,2 +1,5 @@
 pub mod no_light_renderer;
 pub mod whitted_ray_tracer;
+
+pub use no_light_renderer::NoLightRenderer;
+pub use whitted_ray_tracer::WhittedRayTracer;
