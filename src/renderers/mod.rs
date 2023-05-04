e4b78592457e0ee5d3fbefc5ca2772afb01175b5
Adrien Vannson <adrien.vannson@protonmail.com>
2023-05-04 13:46:48 +0200

    Fix no lights renderer

--- src/renderers/mod.rs
+++ src/renderers/mod.rs
@@ -3,5 +3,5 @@ pub mod no_light_renderer;
 pub mod whitted_ray_tracer;
 
 //pub use monte_carlo_renderer::MonteCarloRenderer;
-//pub use no_light_renderer::NoLightRenderer;
+pub use no_light_renderer::NoLightRenderer;
 pub use whitted_ray_tracer::WhittedRayTracer;
--- src/renderers/no_light_renderer.rs
+++ src/renderers/no_light_renderer.rs
@@ -2,12 +2,12 @@ use crate::color::Color;
 use crate::ray::Ray;
 use crate::renderer::Renderer;
 use crate::scene::Scene;
-/*
+
 /// This basic renderer ignores all the lights in the scene: the color of a ray
 /// is simply the color of the object.
 pub struct NoLightRenderer {}
 
-impl Renderer for NoLightRenderer {
+impl NoLightRenderer {
     fn color(&self, ray: Ray, scene: &Scene) -> Color {
         if let Some((primitive, collision)) = scene.collision(ray) {
             let material = primitive.material_at_collition(collision);
@@ -16,4 +16,25 @@ impl Renderer for NoLightRenderer {
             Color::black()
         }
     }
-}*/
+}
+
+impl Renderer for NoLightRenderer {
+    fn render(&self, scene: Scene) -> Vec<Vec<(u8, u8, u8)>> {
+        let mut image: Vec<Vec<(u8, u8, u8)>> =
+            vec![vec![(0, 0, 0); scene.camera.height]; scene.camera.width];
+        
+        for x in 0..scene.camera.width {
+            for y in 0..scene.camera.height {
+                let color = self.color(scene.camera.generate_ray(x, y), &scene);
+
+                image[x][y] = (
+                    (255. * color.red) as u8,
+                    (255. * color.green) as u8,
+                    (255. * color.blue) as u8,
+                );
+            }
+        }
+
+        image
+    }
+}
